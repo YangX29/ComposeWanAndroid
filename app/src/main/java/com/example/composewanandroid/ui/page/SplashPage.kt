ab59423eda4e34e83e9331545ce4119221a13421
YangX29 <1214719907@qq.com>
2022-01-01 20:59:15 +0800

    修改

--- app/src/main/java/com/example/composewanandroid/ui/page/SplashPage.kt
+++ app/src/main/java/com/example/composewanandroid/ui/page/SplashPage.kt
@@ -3,21 +3,17 @@ package com.example.composewanandroid.ui.page
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composewanandroid.R
-import com.example.composewanandroid.base.net.HttpCall
 import com.example.composewanandroid.utils.Constants
 import kotlinx.coroutines.*
 
